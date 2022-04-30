414a259bcee71d990c8df80e50aedd283f657663
manpreet26git <54136034+manpreet26git@users.noreply.github.com>
2022-04-30 10:58:37 +0530

    Update Questionone.java

--- Questionone.java
+++ Questionone.java
@@ -13,7 +13,9 @@ public class Questionone {
     public int getSolutionOfQuestionOne(int [] arr){
 
         HashMap<Integer, Integer> map = new HashMap<>();
-
+        
+        
+        //use hashmap to keep a count of freq of a particular color
         for(int i=0;i<arr.length;i++){
             if(map.containsKey(arr[i])){
                 map.put(arr[i], map.get(arr[i])+1);
@@ -26,6 +28,7 @@ public class Questionone {
         for(int key: map.keySet()){
             int freq = map.get(key);
             ans += getSum(freq-1);
+            // if [1,1,1] the pairs will be of indexes (0,1), (0,2), (1,2)
         }
         return ans;
     }
